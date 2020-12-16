047182644bacdeb2e00d77153b93182d368ee994
lperisic <luka.perisic@gauss.hr>
2020-12-16 20:58:10 +0100

    testing

--- start/routes.js
+++ start/routes.js
@@ -5,6 +5,7 @@ const Route = use('Route')
 
 Route.on('/').render('welcome')
 _requireRoutes('User').prefix('api/users')
+_requireRoutes('Player').prefix('api/players')
 
 function _requireRoutes(group) {
   return require(`../app/Routes/${group}`)
