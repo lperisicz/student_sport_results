ac7f6bab544995a609107080d04d226769c8124f
lperisic <luka.perisic@gauss.hr>
2020-12-18 15:01:05 +0100

    feat: added player create and filter

--- app/Controllers/Http/PlayerController.js
+++ app/Controllers/Http/PlayerController.js
@@ -1,43 +1,12 @@
 'use strict'
+const Player = use("App/Models/Player")
 
 class PlayerController {
 
-  randomNameGenerator (num) {
-    let res = '';
-    for(let i = 0; i < num; i++){
-      const random = Math.floor(Math.random() * 27);
-      res += String.fromCharCode(97 + random);
-    }
-    return res;
-  }
-
-  teams = [
-    {
-      name: this.randomNameGenerator(7),
-      city: this.randomNameGenerator(8)
-    },
-    {
-      name: this.randomNameGenerator(7),
-      city: this.randomNameGenerator(8)
-    }
-  ]
-
   async filter({response}) {
-
-    let players = []
-    for(let i = 0; i < 30; i++) {
-      players[i] = {
-        displayName: this.randomNameGenerator(7) + " " + this.randomNameGenerator(9),
-        team: this.teams[i % 2]
-      }
-    }
-
-    let responseDummyData = {
-      data: {
-        players: players
-      }
-    }
-    response.send(responseDummyData)
+    response.send({
+      data: await Player.all()
+    })
   }
 
   async test({response}) {
@@ -49,6 +18,14 @@ class PlayerController {
       "\n" +
       "</body>\n" +
       "</html>")
+  }
+
+  async create({request, response}) {
+    let payload = request.post();
+    let created = await Player.create(payload)
+    response.send({
+      data: created
+    })
   }
 
 }
--- app/Routes/Player.js
+++ app/Routes/Player.js
@@ -5,8 +5,26 @@ const Route = use('Route')
 
 module.exports = Route.group(() => {
 
+  /**
+   * @api {get} /api/players/filter filter
+   * @apiGroup Player
+   *
+   * @apiDescription Public route lists all players
+   *
+   */
   Route.get('/filter', 'PlayerController.filter')
 
   Route.get('/test', 'PlayerController.test')
+
+  /**
+   * @api {post} /api/players/create create
+   * @apiGroup Player
+   *
+   * @apiDescription Public route create a single player
+   *
+   * @apiParam {string} name Players full name
+   *
+   */
+  Route.post('/create', 'PlayerController.create')
 
 })
